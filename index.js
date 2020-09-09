ebff48bfd31c2e345c9a321817c66c53e28b5807
Dominic Guaragna <51342281+dmgrizz@users.noreply.github.com>
2020-09-09 16:51:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -36,20 +36,23 @@ function Layout(k){
 
 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-coll[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var content = this.nextElementSibling;
-  if (content.style.display === "block") {
-    content.style.display = "none";
-  } else {
-    content.style.display = "block";
-  }
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+//
+// for (i = 0; i < coll.length; i++) {
+// coll[i].addEventListener("click", function() {
+//   this.classList.toggle("active");
+//   var content = this.nextElementSibling;
+//   if (content.style.display === "block") {
+//     content.style.display = "none";
+//   } else {
+//     content.style.display = "block";
+//   }
+// });
+// }
+$("#collapse-btn").on("click", function() {
+  $("#base").slideToggle();
 });
-}
     // JavaScript code to avoid dropdown
     // menu close
 
