56776dcf0680d3faf96f5ea6092d86ba3e3cab65
Dominic Guaragna <51342281+dmgrizz@users.noreply.github.com>
2020-10-01 11:38:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('public', express.static(__dirname + '/index.js'));
 
 
+app.post("/", function(req, res){
 
+  const email = req.body.email;
+  const pass = req.body.password;
+  const rePass = req.body.rePassword;
+  console.log(email, pass, rePass);
+
+  // const data = {
+  //   members:[
+  //   {
+  //     email_address: email,
+  //     status: "subscribed",
+  //     merge_fields:
+  //   }
+  //
+  //   ]
+  // };
+
+});
 
 
 app.listen(3000, function(){
